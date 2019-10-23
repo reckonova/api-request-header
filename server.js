3e74459e68871f6134c6eb5d52cc4c5bdd824f85
Glitch (1fcc-request-header-parser-dcc) <none>
2019-10-23 17:28:11 +0000

    📈👼 request you header on over to Git!

--- server.js
+++ server.js
@@ -1,27 +1,36 @@
-
-var express = require('express');
+var express = require("express");
 var app = express();
 
-// we've started you off with Express, 
-// but feel free to use whatever libs or frameworks you'd like through `package.json`.
 
-// http://expressjs.com/en/starter/static-files.html
-app.use(express.static('public'));
+app.use(express.static("public"));
 
-// http://expressjs.com/en/starter/basic-routing.html
-app.get('/', function(request, response) {
-  response.sendFile(__dirname + '/index.html');
+
+app.get("/", function(req, res, next) {
+  res.sendFile(__dirname + "/index.html");
 });
 
-// listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
+var os = require("os");
+var networkInterfaces = os.networkInterfaces();
+const requestIp = require("request-ip");
+
+app.get("/api/whoami", function(req, res) {
+  const clientIp = requestIp.getClientIp(req);
+  var iface = os.networkInterfaces();
+  var ip = iface.eth0[0].address;
+  
+  
+  
+  
+  res.json({
+    "ipaddress": clientIp,
+    software: req.headers["user-agent"],
+    "language": req.headers["accept-language"].split(",")[0],
+    "system": os.platform() + " " + os.arch()
+  });
 });
-var os = require( 'os' );var networkInterfaces = os.networkInterfaces( );
-const requestIp = require('request-ip');
-app.get('/api/whoami', function (req, res ) {
-const clientIp = requestIp.getClientIp(req);
 
-  res.json({ip:clientIp,software:req.headers['user-agent'], language: req.headers['accept-language']})
-})
 
+var listener = app.listen(process.env.PORT, function() {
+  console.log("Your app is listening on port " + listener.address().port);
+});
+listener;
